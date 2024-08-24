a759abfa5d625d0f54faa137598c230560a8644f
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2024-08-24 18:52:17 +0200

    🔧 Setup transient/temporary queue.

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/sirtaylor88/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/sirtaylor88/learn-pub-sub-starter/internal/pubsub"
+	"github.com/sirtaylor88/learn-pub-sub-starter/internal/routing"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+	const rabbitConnString = "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(rabbitConnString)
+	if err != nil {
+		log.Fatalf("Could not connect to RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+	fmt.Println("Peril game server connected successfully to RabbitMQ !!!")
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalf("Invalid username: %v", err)
+	}
+
+	_, _, err = pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilDirect,
+		routing.PauseKey+"."+username,
+		routing.PauseKey,
+		0,
+	)
+
+	if err != nil {
+		log.Printf("Could not declare and bind the queue to exchange: %v", err)
+	}
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Connection is shutting down...")
 }
--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -3,6 +3,7 @@ package pubsub
 import (
 	"context"
 	"encoding/json"
+	"log"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
@@ -20,4 +21,39 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	ctx := context.Background()
 
 	return ch.PublishWithContext(ctx, exchange, key, false, false, msg)
+}
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	simpleQueueType int, // an enum to represent "durable" or "transient"
+) (*amqp.Channel, amqp.Queue, error) {
+	pubChannel, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Could not create new channel: %v", err)
+	}
+	var durable, autoDelete, exclusive bool
+
+	if simpleQueueType == 1 {
+		durable = true
+		autoDelete = false
+		exclusive = false
+	} else {
+		durable = false
+		autoDelete = true
+		exclusive = true
+	}
+	pubQueue, err := pubChannel.QueueDeclare(
+		queueName, durable, autoDelete, exclusive, false, nil,
+	)
+	if err != nil {
+		log.Fatalf("Could not create queue: %v", err)
+	}
+	err = pubChannel.QueueBind(queueName, key, exchange, false, nil)
+	if err != nil {
+		log.Fatalf("Could not create new channel: %v", err)
+	}
+	return pubChannel, pubQueue, nil
 }
